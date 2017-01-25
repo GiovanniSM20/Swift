88e11a9b628023adbda0ae697ca3dc726c2fb8b1
Giovanni Martins <giovannimartins2000@gmail.com>
2017-01-25 21:25:31 -0200

    putting the sum of values and declaration of variables with type and not type

--- meetingSwift.playground/Contents.swift
+++ meetingSwift.playground/Contents.swift
@@ -4,4 +4,23 @@ import UIKit
 
 var str = "Hello, playground"
 
-var number = 1 + 2
+// String
+var userName = "Giovanni"
+userName = "Innavoig"
+
+// Number Integer
+var numberInteger = 20
+var integerNumber: Int = 10
+
+// Sum of values
+// The variable is 20
+integerNumber = integerNumber + 10
+
+// The variable is 25
+integerNumber = integerNumber + 5
+
+// The variable is 50
+integerNumber = integerNumber * 2
+
+// And now, the variable is 0
+integerNumber = 0
